5d6dd920b6f361828b5a4f1dc25dcdaa09a59a97
Filip Dujmusic <filip@ampnet.io>
2022-12-03 01:37:51 +0100

    fix wallet connect modal

--- src/core/actions/WalletAuthorizationAction.ts
+++ src/core/actions/WalletAuthorizationAction.ts
@@ -27,9 +27,9 @@ export class WalletAuthorizationAction {
     public present(): Promise<User> {
         ensureBrowser();
         let div = document.createElement('div');
-        div.setAttribute("style", "position:fixed;top:0;left:0;background:rgba(0,0,0,0.6);z-index:1;width:100%;height:100%;pointer-events:none;display:block;");
+        div.setAttribute("style", "position:fixed;top:0;left:0;background:rgba(0,0,0,0.6);z-index:1;width:100%;height:100%;display:block;padding-top:32px;");
         div.innerHTML = `
-            <iframe style="width:80%;margin:auto;" src="${this.actionUrl}" scrolling="no" frameborder="0px"></iframe>
+            <iframe style="width:100%;margin:auto;max-width:500px;display:block;border:none;border-radius:16px;height:100%;max-height:700px;" src="${this.actionUrl}" scrolling="no" frameborder="0px"></iframe>
         `;
         document.body.appendChild(div);
         return new Promise<User>((resolve, reject) => {
